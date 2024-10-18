c82b774cc0d443c1ff27c917b1e816119337ad04
Kirill Yermolaev <lirik98@mail.ru>
2024-10-18 16:41:32 +0300

    fix: webpack

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const TG_WEBAPP = window.Telegram.WebApp;
 
 TG_WEBAPP.ready();
 console.log("Hello World!", TG_WEBAPP.initDataUnsafe);
+
+TG_WEBAPP.sendData(JSON.stringify({
+    test: "ffffff"
+}))
