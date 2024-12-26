166b3dcae3d26633f4e079a7f2f391f6e5a1db49
Кирилл Ермолаев <kirill.yermolaev98@gmail.com>
2024-12-26 17:00:26 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const TG_WEBAPP = window.Telegram.WebApp;
 
 console.log("Hello World!", TG_WEBAPP);
 
+const FFF = document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+FFF.innerHTML = window.location.origin;
+
 setTimeout(() => {
     // TG_WEBAPP.sendData(
     //     JSON.stringify({
